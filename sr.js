8bbc50f36670eadb07c4ddc4745bfb9588eb46ce
sh5010 <156681312+sh5010@users.noreply.github.com>
2024-12-24 15:27:01 +0300

    Add files via upload

---
+++ sr.js
@@ -0,0 +1,30 @@
+{
+    let x=5; 
+    var y=true; // global 
+    const z=20;
+    console.log(typeof(y));
+
+   
+}
+
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple })
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
